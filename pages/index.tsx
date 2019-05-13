9b80440ac2cd46a20a7cdb1372c6f9548475f5c5
AndrewRademacher <andrewrademacher@icloud.com>
2019-05-12 21:23:08 -0500

    Init project.

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const IndexPage = () => (
+    <>
+        <h1>Entry Form</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est laborum nulla pariatur quo reiciendis? Ab ad
+            atque ea ex illo maxime modi reiciendis repudiandae saepe, sequi suscipit ut vitae voluptatum.</p>
+    </>
+);
+
+export default IndexPage;
