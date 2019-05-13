70985dcfd2e40f18625ff21ef7c167f5d7271f27
AndrewRademacher <andrewrademacher@icloud.com>
2019-05-12 21:41:06 -0500

    Model component state traditionally.

---
+++ components/counter/index.tsx
@@ -0,0 +1,74 @@
+import * as React from 'react';
+
+export interface Props {
+    initialValue?: number
+}
+
+interface State {
+    value: number
+}
+
+export class Counter extends React.Component<Props, State> {
+
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            value: props.initialValue || 0,
+        };
+    }
+
+    handleIncrement = (): void => {
+        this.setState(state => {
+            return {
+                value: state.value + 1,
+            };
+        });
+    };
+
+    handleDecrement = (): void => {
+        this.setState(state => {
+            return {
+                value: state.value - 1,
+            };
+        });
+    };
+
+    render(): React.ReactNode {
+        const { value } = this.state;
+        return (
+            <div className="counter">
+                <div className="counter__button" onClick={this.handleDecrement}>-</div>
+                <div className="counter__value">{value}</div>
+                <div className="counter__button" onClick={this.handleIncrement}>+</div>
+
+                {/*language=SCSS*/}
+                <style jsx>{`
+                    .counter {
+                        display: flex;
+                        flex-flow: row nowrap;
+                        align-items: center;
+                    }
+
+                    .counter__button {
+                        padding: .5rem;
+                        background: blue;
+                        color: white;
+                        border-radius: 5px;
+                    }
+                    
+                    .counter__value {
+                        padding: .5rem;
+                    }
+                    
+                    .counter__button:hover {
+                        background: darkblue;
+                    }
+                    
+                    .counter__buton:active {
+                        background: darkblue;
+                    }
+                `}</style>
+            </div>
+        );
+    }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
+import { Counter } from '../components/counter';
 
 const IndexPage = () => (
     <>
-        <h1>Entry Form</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est laborum nulla pariatur quo reiciendis? Ab ad
-            atque ea ex illo maxime modi reiciendis repudiandae saepe, sequi suscipit ut vitae voluptatum.</p>
+        <h1>Counter</h1>
+        <Counter/>
     </>
 );
 
